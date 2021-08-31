cdc3aca428ab8d6351a6147360acfa427d28ac92
DO THI QUYNH CHI <1721050327@student.humg.edu.vn>
2021-08-31 13:47:54 +0700

    BT1

---
+++ GPTB1/GPTB1/Controllers/GiaiptrinhController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GPTB1.Controllers
+{
+    public class GiaiptrinhController : Controller
+    {
+        // GET: gpt
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GPTB1( double soa, double sob)
+        {
+            double x = 0;
+            x = -sob/sob ;
+            
+            return View();
+        }
+    }
+}
